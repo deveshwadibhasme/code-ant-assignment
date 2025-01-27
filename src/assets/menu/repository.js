e7eed60034e3c35f492846458815758dd8d05554
Devesh Wadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2025-01-27 17:20:39 +0530

    feature/linkToRepo

--- src/assets/menu/repository.js
+++ src/assets/menu/repository.js
@@ -4,204 +4,241 @@ const Repository = [
         "size": 203,
         "visibility": "public",
         "majorCode": "CSS",
-        "id": 897136132
+        "id": 897136132,
+        "htmlUrl": "https://github.com/deveshwadibhasme/23LandingPageOasis"
     },
     {
         "repoName": "boatClone",
         "size": 164,
         "visibility": "public",
         "majorCode": "HTML",
-        "id": 827318155
+        "id": 827318155,
+        "htmlUrl": "https://github.com/deveshwadibhasme/boatClone"
     },
     {
         "repoName": "calculator-noBugs-",
         "size": 2,
         "visibility": "public",
         "majorCode": "HTML",
-        "id": 841322286
+        "id": 841322286,
+        "htmlUrl": "https://github.com/deveshwadibhasme/calculator-noBugs-"
     },
     {
         "repoName": "checkSlot",
         "size": 18513,
         "visibility": "public",
         "majorCode": "CSS",
-        "id": 815577421
-    },
-    {
-        "repoName": "country-api",
-        "size": 79,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 896697974
-    },
-    {
-        "repoName": "country-api-project",
-        "size": 98,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 852046598
-    },
-    {
-        "repoName": "desertWebsite",
-        "size": 1030,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 885797736
-    },
-    {
-        "repoName": "leadership-landing",
-        "size": 233,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 899577353
-    },
-    {
-        "repoName": "mini-project-css-1",
-        "size": 282,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 821923277
-    },
-    {
-        "repoName": "mini-project-css-2",
-        "size": 45,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 823110960
-    },
-    {
-        "repoName": "notes-mania",
-        "size": 309,
-        "visibility": "public",
-        "majorCode": "HTML",
-        "id": 887220206
-    },
-    {
-        "repoName": "oasis-landing-react",
-        "size": 452,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 902263318
-    },
-    {
-        "repoName": "OISIP",
-        "size": 4,
-        "visibility": "public",
-        "majorCode": null,
-        "id": 903221481
-    },
-    {
-        "repoName": "orangewit",
-        "size": 531,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 841803207
-    },
-    {
-        "repoName": "photos-folk",
-        "size": 3010,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 868712847
-    },
-    {
-        "repoName": "pokemonindia",
-        "size": 1214,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 831252513
-    },
-    {
-        "repoName": "port-folio",
-        "size": 4004,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 814307481
-    },
-    {
-        "repoName": "portfolio-updated",
-        "size": 4978,
-        "visibility": "public",
-        "majorCode": "JavaScript",
-        "id": 882105491
-    },
-    {
-        "repoName": "pricing-component",
-        "size": 18,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 875371420
-    },
-    {
-        "repoName": "receipe-app",
-        "size": 93,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 904357079
-    },
-    {
-        "repoName": "retech",
-        "size": 975,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 856221074
-    },
-    {
-        "repoName": "Svelte",
-        "size": 3534,
-        "visibility": "public",
-        "majorCode": "C++",
-        "id": 846673953
-    },
-    {
-        "repoName": "temp-converter",
-        "size": 41,
-        "visibility": "public",
-        "majorCode": "JavaScript",
-        "id": 898630563
-    },
-    {
-        "repoName": "todo",
-        "size": 50,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 858911807
-    },
-    {
-        "repoName": "todo-react",
-        "size": 1988,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 900652704
-    },
-    {
-        "repoName": "todo-react-app",
-        "size": 1971,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 899146849
-    },
-    {
-        "repoName": "ToDoUpdated",
-        "size": 1779,
-        "visibility": "public",
-        "majorCode": "JavaScript",
-        "id": 883667705
-    },
-    {
-        "repoName": "twt-services-updated",
-        "size": 7669,
-        "visibility": "public",
-        "majorCode": "CSS",
-        "id": 880273890
+        "id": 815577421,
+        "htmlUrl": "https://github.com/deveshwadibhasme/checkSlot"
     },
     {
         "repoName": "code-ant-assignment",
         "size": 244,
         "visibility": "public",
         "majorCode": "JavaScript",
-        "id": 905875057
+        "id": 905875057,
+        "htmlUrl": "https://github.com/deveshwadibhasme/code-ant-assignment"
     },
+    {
+        "repoName": "country-api",
+        "size": 79,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 896697974,
+        "htmlUrl": "https://github.com/deveshwadibhasme/country-api"
+    },
+    {
+        "repoName": "country-api-project",
+        "size": 98,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 852046598,
+        "htmlUrl": "https://github.com/deveshwadibhasme/country-api-project"
+    },
+    {
+        "repoName": "desertWebsite",
+        "size": 1030,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 885797736,
+        "htmlUrl": "https://github.com/deveshwadibhasme/desertWebsite"
+    },
+    {
+        "repoName": "leadership-landing",
+        "size": 233,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 899577353,
+        "htmlUrl": "https://github.com/deveshwadibhasme/leadership-landing"
+    },
+    {
+        "repoName": "mini-project-css-1",
+        "size": 282,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 821923277,
+        "htmlUrl": "https://github.com/deveshwadibhasme/mini-project-css-1"
+    },
+    {
+        "repoName": "mini-project-css-2",
+        "size": 45,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 823110960,
+        "htmlUrl": "https://github.com/deveshwadibhasme/mini-project-css-2"
+    },
+    {
+        "repoName": "notes-mania",
+        "size": 309,
+        "visibility": "public",
+        "majorCode": "HTML",
+        "id": 887220206,
+        "htmlUrl": "https://github.com/deveshwadibhasme/notes-mania"
+    },
+    {
+        "repoName": "oasis-landing-react",
+        "size": 452,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 902263318,
+        "htmlUrl": "https://github.com/deveshwadibhasme/oasis-landing-react"
+    },
+    {
+        "repoName": "OISIP",
+        "size": 4,
+        "visibility": "public",
+        "majorCode": null,
+        "id": 903221481,
+        "htmlUrl": "https://github.com/deveshwadibhasme/OISIP"
+    },
+    {
+        "repoName": "orangewit",
+        "size": 531,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 841803207,
+        "htmlUrl": "https://github.com/deveshwadibhasme/orangewit"
+    },
+    {
+        "repoName": "photos-folk",
+        "size": 3010,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 868712847,
+        "htmlUrl": "https://github.com/deveshwadibhasme/photos-folk"
+    },
+    {
+        "repoName": "pokemonindia",
+        "size": 1214,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 831252513,
+        "htmlUrl": "https://github.com/deveshwadibhasme/pokemonindia"
+    },
+    {
+        "repoName": "port-folio",
+        "size": 4004,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 814307481,
+        "htmlUrl": "https://github.com/deveshwadibhasme/port-folio"
+    },
+    {
+        "repoName": "portfolio-updated",
+        "size": 4978,
+        "visibility": "public",
+        "majorCode": "JavaScript",
+        "id": 882105491,
+        "htmlUrl": "https://github.com/deveshwadibhasme/portfolio-updated"
+    },
+    {
+        "repoName": "pricing-component",
+        "size": 18,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 875371420,
+        "htmlUrl": "https://github.com/deveshwadibhasme/pricing-component"
+    },
+    {
+        "repoName": "receipe-app",
+        "size": 93,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 904357079,
+        "htmlUrl": "https://github.com/deveshwadibhasme/receipe-app"
+    },
+    {
+        "repoName": "retech",
+        "size": 975,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 856221074,
+        "htmlUrl": "https://github.com/deveshwadibhasme/retech"
+    },
+    {
+        "repoName": "Svelte",
+        "size": 3534,
+        "visibility": "public",
+        "majorCode": "C++",
+        "id": 846673953,
+        "htmlUrl": "https://github.com/deveshwadibhasme/Svelte"
+    },
+    {
+        "repoName": "temp-converter",
+        "size": 41,
+        "visibility": "public",
+        "majorCode": "JavaScript",
+        "id": 898630563,
+        "htmlUrl": "https://github.com/deveshwadibhasme/temp-converter"
+    },
+    {
+        "repoName": "todo",
+        "size": 50,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 858911807,
+        "htmlUrl": "https://github.com/deveshwadibhasme/todo"
+    },
+    {
+        "repoName": "todo-react",
+        "size": 1988,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 900652704,
+        "htmlUrl": "https://github.com/deveshwadibhasme/todo-react"
+    },
+    {
+        "repoName": "todo-react-app",
+        "size": 1971,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 899146849,
+        "htmlUrl": "https://github.com/deveshwadibhasme/todo-react-app"
+    },
+    {
+        "repoName": "ToDoUpdated",
+        "size": 1779,
+        "visibility": "public",
+        "majorCode": "JavaScript",
+        "id": 883667705,
+        "htmlUrl": "https://github.com/deveshwadibhasme/ToDoUpdated"
+    },
+    {
+        "repoName": "twt-services-updated",
+        "size": 7669,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 880273890,
+        "htmlUrl": "https://github.com/deveshwadibhasme/twt-services-updated"
+    },
+    {
+        "repoName": "TWTServices",
+        "size": 9587,
+        "visibility": "public",
+        "majorCode": "CSS",
+        "id": 844672786,
+        "htmlUrl": "https://github.com/deveshwadibhasme/TWTServices"
+    }
 ]
 
 export default Repository
